ad931d36c033c489be7b4b4e4ccf2d616f102c9f
Joenel <joenelsevellejo831@gmail.com>
2024-08-01 18:48:43 +0800

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export const metadata: Metadata = {
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+        </body>
     </html>
   );
 }
